d32546755d4dec7b95b417f01dc2a209b8592c2e
hjoon <guswns3371@gmail.com>
2021-02-15 23:02:19 +0900

    Initial commit

---
+++ src/main/java/com/example/dockertest/Member.java
@@ -0,0 +1,26 @@
+package com.example.dockertest;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@ToString
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memberId")
+    private Long memberId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Builder
+    public Member(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/dockertest/TestController.java
@@ -0,0 +1,27 @@
+package com.example.dockertest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/save")
+    public void test(){
+        Member m1 = new Member("m1");
+        memberRepository.save(m1);
+    }
+
+    @GetMapping("/get")
+    public String getM1() {
+        return memberRepository.findAll().toString();
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "docker hello";
+    }
+}
