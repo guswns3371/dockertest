e346fa7ed221d12329b5662c5a538873f35d2499
guswns3371 <guswns3371@gmail.com>
2021-02-20 04:21:39 +0900

    [TEST] spring logging test

--- src/main/java/com/example/dockertest/TestController.java
+++ src/main/java/com/example/dockertest/TestController.java
@@ -1,12 +1,18 @@
 package com.example.dockertest;
 
+import com.mysql.cj.log.LogFactory;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final MemberRepository memberRepository;
 
     @GetMapping("/save")
@@ -27,6 +33,12 @@ public class TestController {
 
     @GetMapping("/v2")
     public String docker_jenkins() {
+        logger.trace("Trace Level 테스트");
+        logger.debug("DEBUG Level 테스트");
+        logger.info("INFO Level 테스트");
+        logger.warn("Warn Level 테스트");
+        logger.error("ERROR Level 테스트");
+
         return "docker jenkins success!!";
     }
 }
